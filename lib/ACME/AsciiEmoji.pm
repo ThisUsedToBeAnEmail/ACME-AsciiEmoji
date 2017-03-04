5feb802fe219e2a78a0d3e7b660537fc841278b1
LNATION <thisusedtobeanemail@gmail.com>
2017-03-04 16:54:07 +0100

    emojis

--- lib/ACME/AsciiEmoji.pm
+++ lib/ACME/AsciiEmoji.pm
@@ -7,28 +7,186 @@ use Data::Dumper;
 use Exporter::Shiny;
 
 my %emojis = (
-    innocent => [   
-	      202,
-          152,
-          226,
-          128,
-          191,
-          202,
-          152
+    innocent    => [ 202, 152, 226, 128, 191, 202, 152 ],
+    disapproval => [ 224, 178, 160, 32,  95,  224, 178, 160 ],
+    table_flip => [
+        40,  226, 149, 175, 194, 176, 226, 150, 161, 194,
+        176, 239, 188, 137, 226, 149, 175, 239, 184, 181,
+        32,  226, 148, 187, 226, 148, 129, 226, 148, 187
     ],
-    disapproval => [
-          224,
-          178,
-          160,
-          32,
-          95,
-          224,
-          178,
-          160
+    put_the_table_back => [
+        226, 148, 172, 226, 148, 128, 226, 148, 172, 227, 131, 142, 40, 32,
+        227, 130, 156, 45,  227, 130, 156, 227, 131, 142, 41
+    ],
+    double_flip => [
+        226, 148, 187, 226, 148, 129, 226, 148, 187, 239, 184, 181, 227, 131,
+        189, 40,  96,  208, 148, 194, 180, 41,  239, 190, 137, 239, 184, 181,
+        226, 148, 187, 226, 148, 129, 226, 148, 187
+    ],
+    super_waving => [
+        40,  32,  239, 190, 159, 226, 136, 128, 239, 190, 159, 41,  239, 189,
+        177, 239, 190, 138, 239, 190, 138, 229, 133, 171, 229, 133, 171, 239,
+        190, 137, 227, 131, 189, 239, 190, 137, 227, 131, 189, 239, 190, 137,
+        227, 131, 189, 239, 190, 137, 32,  239, 188, 188, 32,  47,  32,  239,
+        188, 188, 47,  32,  239, 188, 188
+    ],
+    fistacuffs => [
+        225, 131, 154, 40,  239, 189, 128, 227,
+        131, 188, 194, 180, 225, 131, 154, 41
+    ],
+    cute_bear => [ 
+        202, 149, 226, 128, 162, 225, 180, 165, 226, 128, 162, 202, 148 
+    ],
+    big_eyes => [
+        40,  239, 189, 161, 226, 151, 149, 226, 128, 191,
+        226, 151, 149, 239, 189, 161, 41
+    ],
+    surprised => [
+        239, 188, 136, 227, 128, 128, 239, 190, 159, 208,
+        148, 239, 190, 159, 239, 188, 137
+    ],
+    shrug => [ 194, 175, 92, 95, 40,  227, 131, 132, 41, 95, 47,  194, 175 ],
+    meh   => [ 194, 175, 92, 40, 194, 176, 95,  111, 41, 47, 194, 175 ],
+    feel_perky => [ 
+        40, 96, 239, 189, 165, 207, 137, 239, 189, 165, 194, 180, 41 
+    ],
+    angry => [
+        40,  226, 149, 172, 32,  224, 178, 160, 32, 231,
+        155, 138, 224, 178, 160, 32,  41
+    ],
+    excited => [
+        226, 152, 156, 40,  226, 140, 146, 32,  226, 150,
+        189, 226, 140, 146, 32,  41,  226, 152, 158
+    ],
+    running => [
+        206, 181, 61, 206, 181, 61,  206, 181, 61, 226, 148, 140,
+        40,  59,  42, 194, 180, 208, 148, 96,  41, 239, 190, 137
+    ],
+    happy => [ 227, 131, 189, 40, 194, 180, 226, 150, 189, 32, 96, 41, 47 ],
+    basking_in_glory => [
+        227, 131, 189, 40,  194, 180, 227, 131,
+        188, 239, 189, 128, 41,  227, 131, 142
+    ],
+    kitty => [ 225, 181, 146, 225, 180, 165, 225, 181, 146, 35 ],
+    meow  => [
+        224, 184, 133, 94, 226, 128, 162, 239, 187, 140,
+        226, 128, 162, 94, 224, 184, 133
+    ],
+    cheers => [
+        239, 188, 136, 32,  94,  95,  94,  239, 188, 137, 111, 232, 135, 170,
+        232, 135, 170, 111, 239, 188, 136, 94,  95,  94,  32,  239, 188, 137
+    ],
+    chan => [
+        40,  32, 205, 161, 194, 176, 32, 205, 156, 202,
+        150, 32, 205, 161, 194, 176, 41
+    ],
+    disagree => [ 217, 169, 226, 151, 148, 204, 175, 226, 151, 148, 219, 182 ],
+    flexing  => [
+        225, 149, 153, 40,  226, 135, 128, 226, 128, 184,
+        226, 134, 188, 226, 128, 182, 41,  225, 149, 151
+    ],
+    do_you_lift_bro => [
+        225, 149, 166, 40, 195, 178, 95, 195, 179, 203, 135, 41, 225, 149, 164
+    ],
+    'kirby' => [
+        226, 138, 130, 40, 226, 151, 137, 226, 128, 191,
+        226, 151, 137, 41, 227, 129, 164
+    ],
+    'tripping_out' => [ 
+        113, 40, 226, 157, 130, 226, 128, 191, 226, 157, 130, 41, 112 
+    ],
+    'discombobulated' => [ 226, 138, 153, 239, 185, 143, 226, 138, 153 ],
+    'sad_shrug'       => [
+        194, 175, 92,  95,  40,  226, 138, 153, 32,  239,
+        184, 191, 226, 138, 153, 41,  95,  47,  194, 175
+    ],
+    'confused' => [ 194, 191, 226, 147, 167, 95, 226, 147, 167, 239, 174, 140 ],
+    'confused_scratch' => [ 40, 226, 138, 153, 46, 226, 152, 137, 41, 55 ],
+    'worried' => [ 40, 194, 180, 239, 189, 165, 95, 239, 189, 165, 96, 41 ],
+    'dear_god_why' => [
+        209, 137, 239, 188, 136, 239, 190, 159, 208, 148,
+        239, 190, 159, 209, 137, 239, 188, 137
+    ],
+    'staring' => [
+        217, 169, 40, 205, 161, 224, 185, 143, 95, 224, 185, 143, 41, 219, 182
+    ],
+    'strut' => [ 225, 149, 149, 40, 32, 225, 144, 155, 32, 41, 225, 149, 151 ],
+    'zoned' => [ 40, 226, 138, 153, 95, 226, 151, 142, 41 ],
+    'crazy' => [
+        227, 131, 159, 226, 151, 143, 239, 185,
+        143, 226, 152, 137, 227, 131, 159
+    ],
+    'trolling' => [
+        32,  224, 188, 188, 226, 136, 181, 224, 188, 189, 32,  224, 188, 188,
+        226, 141, 168, 224, 188, 189, 32,  224, 188, 188, 226, 141, 162, 224,
+        188, 189, 32,  224, 188, 188, 226, 141, 164, 224, 188, 189
+    ],
+    'angry_troll' => [
+        227, 131, 189, 224, 188, 188, 32,  224, 178, 160, 231, 155,
+        138, 224, 178, 160, 32,  224, 188, 189, 239, 190, 137
+    ],
+    'hugger' => [
+        40,  227, 129, 165, 239, 191, 163, 32, 194, 179,
+        239, 191, 163, 41,  227, 129, 165
+    ],
+    'stranger_danger' => [
+        40,  227, 129, 165, 239, 189, 161, 226, 151, 149, 226, 128, 191, 226,
+        128, 191, 226, 151, 149, 239, 189, 161, 41,  227, 129, 165
+    ],
+    'flip_friend' => [
+        40,  227, 131, 142, 224, 178, 160, 32,  226, 136, 169, 224,
+        178, 160, 41,  227, 131, 142, 229, 189, 161, 40,  32,  92,
+        111, 194, 176, 111, 41,  92,  32
+    ],
+    'cry' => [
+        239, 189, 161, 239, 190, 159, 40,  32,  239, 190,
+        159, 224, 174, 135, 226, 128, 184, 224, 174, 135,
+        239, 190, 159, 41,  239, 190, 159, 239, 189, 161
+    ],
+    'tgif' => [
+        226, 128, 156, 227, 131, 189, 40,  194, 180, 226, 150, 189,
+        239, 189, 128, 41,  227, 131, 142, 226, 128, 157
+    ],
+    'dancing' => [
+        226, 148, 140, 40,  227, 134, 134, 227,
+        137, 168, 227, 134, 134, 41,  202, 131
+    ],
+    'sleepy' => [ 235, 136, 136, 95, 235, 136, 136 ],
+    'shy'    => [
+        40,  224, 185, 145, 226, 128, 162, 204, 129, 32,  226, 130,
+        131, 32,  226, 128, 162, 204, 128, 224, 185, 145, 41
+    ],
+    'fly_away' => [
+        226, 129, 189, 226, 129, 189, 224, 172, 152, 40,
+        32,  203, 138, 225, 181, 149, 203, 139, 32,  41,
+        224, 172, 147, 226, 129, 190, 226, 129, 190
+    ],
+    'careless' => [ 226, 151, 148, 95,  226, 151, 148 ],
+    'love'     => [ 226, 153, 165, 226, 128, 191, 226, 153, 165 ],
+    'touchy' => [
+        212, 133, 40,  226, 137, 150, 226, 128,
+        191, 226, 137, 150, 212, 133, 41
+    ],
+    'robot' => [ 123, 226, 128, 162, 204, 131, 95, 226, 128, 162, 204, 131, 125 ],
+    'seal'         => [ 40, 225, 181, 148, 225, 180, 165, 225, 181, 148, 41 ],
+    'questionable' => [ 40, 212, 190, 226, 128, 184, 32,  212, 190, 41 ],
+    'winning'      => [
+        40,  226, 128, 162, 204, 128, 225, 180, 151, 226, 128, 162,
+        204, 129, 41,  217, 136, 32,  204, 145, 204, 145
+    ],
+    'zombie'   => [ 91, 194, 172, 194, 186, 45, 194, 176, 93, 194, 172 ],
+    'pointing' => [
+        40,  226, 152, 158, 239, 190, 159, 227, 131, 174,
+        239, 190, 159, 41,  226, 152, 158
+    ],
+    'chasing' => [
+        39,  39,  226, 140, 144, 40,  224, 178, 160, 219,
+        190, 224, 178, 160, 41,  194, 172, 39,  39,  39
     ],
 );
 
-our @EXPORT = keys %emojis;
+our @EXPORT    = keys %emojis;
+our @EXPORT_OK = keys %emojis;
 
 =head1 NAME
 
@@ -49,25 +207,251 @@ Quick summary of what the module does.
 Perhaps a little code snippet.
 
     use ACME::AsciiEmoji;
-
     my $foo = ACME::AsciiEmoji->new();
     ...
 
 =cut
 
 sub ascii_emoji {
-	my $emoj = $emojis{$_[0]};
-	return pack('A1' x scalar @{ $emoj }, map { chr($_) } @{ $emoj });
-}	
+    my $emoj = $emojis{ $_[0] };
+    return pack( 'A1' x scalar @{$emoj}, map { chr($_) } @{$emoj} );
+}
 
 sub innocent {
-	return ascii_emoji('innocent');
+    return ascii_emoji('innocent');
 }
 
 sub disapproval {
-	return ascii_emoji('disapproval');
+    return ascii_emoji('disapproval');
 }
 
+sub table_flip {
+    return ascii_emoji('table_flip');
+}
+
+sub put_the_table_back {
+    return ascii_emoji('put_the_table_back');
+}
+
+sub double_flip {
+    return ascii_emoji('double_flip');
+}
+
+sub super_waving {
+    return ascii_emoji('super_waving');
+}
+
+sub fistacuffs {
+    return ascii_emoji('fistacuffs');
+}
+
+sub cute_bear {
+    return ascii_emoji('cute_bear');
+}
+
+sub big_eyes {
+    return ascii_emoji('big_eyes');
+}
+
+sub surprised {
+    return ascii_emoji('surprised');
+}
+
+sub shrug {
+    return ascii_emoji('shrug');
+}
+
+sub meh {
+    return ascii_emoji('meh');
+}
+
+sub feel_perky {
+    return ascii_emoji('feel_perky');
+}
+
+sub angry {
+    return ascii_emoji('angry');
+}
+
+sub excited {
+    return ascii_emoji('excited');
+}
+
+sub running {
+    return ascii_emoji('running');
+}
+
+sub happy {
+    return ascii_emoji('happy');
+}
+
+sub basking_in_glory {
+    return ascii_emoji('basking_in_glory');
+}
+
+sub kitty {
+    return ascii_emoji('kitty');
+}
+
+sub meow {
+    return ascii_emoji('meow');
+}
+
+sub cheers {
+    return ascii_emoji('cheers');
+}
+
+sub devious {
+    return ascii_emoji('devious');
+}
+
+sub chan {
+    return ascii_emoji('chan');
+}
+
+sub disagree {
+    return ascii_emoji('disagree');
+}
+
+sub flexing {
+    return ascii_emoji('flexing');
+}
+
+sub do_you_lift_bro {
+    return ascii_emoji('do_you_lift_bro');
+}
+
+sub kirby {
+    return ascii_emoji('kirby');
+}
+
+sub tripping_out {
+    return ascii_emoji('tripping_out');
+}
+
+sub discombobulated {
+    return ascii_emoji('discombobulated');
+}
+
+sub sad_shrug {
+    return ascii_emoji('sad_shrug');
+}
+
+sub confused {
+    return ascii_emoji('confused');
+}
+
+sub confused_scratch {
+    return ascii_emoji('confused_scratch');
+}
+
+sub worried {
+    return ascii_emoji('worried');
+}
+
+sub dear_god_why {
+    return ascii_emoji('dear_god_why');
+}
+
+sub staring {
+    return ascii_emoji('staring');
+}
+
+sub strut {
+    return ascii_emoji('strut');
+}
+
+sub zoned {
+    return ascii_emoji('zoned');
+}
+
+sub crazy {
+    return ascii_emoji('crazy');
+}
+
+sub trolling {
+    return ascii_emoji('trolling');
+}
+
+sub angry_troll {
+    return ascii_emoji('angry_troll');
+}
+
+sub hugger {
+    return ascii_emoji('hugger');
+}
+
+sub stranger_danger {
+    return ascii_emoji('stranger_danger');
+}
+
+sub flip_friend {
+    return ascii_emoji('flip_friend');
+}
+
+sub cry {
+    return ascii_emoji('cry');
+}
+
+sub tgif {
+    return ascii_emoji('tgif');
+}
+
+sub dancing {
+    return ascii_emoji('dancing');
+}
+
+sub sleepy {
+    return ascii_emoji('sleepy');
+}
+
+sub fly_away {
+    return ascii_emoji('fly_away');
+}
+
+sub careless {
+    return ascii_emoji('careless');
+}
+
+sub love {
+    return ascii_emoji('love');
+}
+
+sub touchy {
+    return ascii_emoji('touchy');
+}
+
+sub robot {
+    return ascii_emoji('robot');
+}
+
+sub seal {
+    return ascii_emoji('seal');
+}
+
+sub questionable {
+    return ascii_emoji('questionable');
+}
+
+sub winning {
+    return ascii_emoji('winning');
+}
+
+sub zombie {
+    return ascii_emoji('zombie');
+}
+
+sub pointing {
+    return ascii_emoji('pointing');
+}
+
+sub chasing {
+    return ascii_emoji('chasing');
+}
+
+sub shy {
+    return ascii_emoji('shy');
+}
 
 =head1 EXPORT
 
@@ -160,4 +544,4 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =cut
 
-1; # End of ACME::AsciiEmoji
+1;    # End of ACME::AsciiEmoji
--- t/01-emoji.t
+++ t/01-emoji.t
@@ -7,11 +7,328 @@ moon_test_one(
     expected => 'ʘ‿ʘ',
 );
 
-
 moon_test_one(
     test => 'scalar',
     meth => \&ACME::AsciiEmoji::disapproval,
     expected => 'ಠ _ಠ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::table_flip,
+    expected => '(╯°□°）╯︵ ┻━┻',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::put_the_table_back,
+    expected => '┬─┬ノ( ゜-゜ノ)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::double_flip,
+    expected => '┻━┻︵ヽ(`Д´)ﾉ︵┻━┻',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::super_waving,
+    expected => '( ﾟ∀ﾟ)ｱﾊﾊ八八ﾉヽﾉヽﾉヽﾉ ＼ / ＼/ ＼',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::fistacuffs,
+    expected => 'ლ(｀ー´ლ)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::cute_bear,
+    expected => 'ʕ•ᴥ•ʔ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::big_eyes,
+    expected => '(｡◕‿◕｡)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::surprised,
+    expected => '（　ﾟДﾟ）',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::shrug,
+    expected => '¯\_(ツ)_/¯',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::meh,
+    expected => '¯\(°_o)/¯',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::feel_perky,
+    expected => '(`･ω･´)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::angry,
+    expected => '(╬ ಠ 益ಠ )',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::excited,
+    expected => '☜(⌒ ▽⌒ )☞',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::running,
+    expected => 'ε=ε=ε=┌(;*´Д`)ﾉ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::happy,
+    expected => 'ヽ(´▽ `)/',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::basking_in_glory,
+    expected => 'ヽ(´ー｀)ノ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::kitty,
+    expected => 'ᵒᴥᵒ#',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::meow,
+    expected => 'ฅ^•ﻌ•^ฅ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::cheers,
+    expected => '（ ^_^）o自自o（^_^ ）',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::chan,
+    expected => '( ͡° ͜ʖ ͡°)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::disagree,
+    expected => '٩◔̯◔۶',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::do_you_lift_bro,
+    expected => 'ᕦ(ò_óˇ)ᕤ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::kirby,
+    expected => '⊂(◉‿◉)つ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::tripping_out,
+    expected =>'q(❂‿❂)p'
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::discombobulated,
+    expected =>'⊙﹏⊙',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::sad_shrug,
+    expected => '¯\_(⊙ ︿⊙)_/¯',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::confused,
+    expected => '¿ⓧ_ⓧﮌ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::confused_scratch,
+    expected => '(⊙.☉)7',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::dear_god_why,
+    expected => 'щ（ﾟДﾟщ）',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::strut,
+    expected => 'ᕕ( ᐛ )ᕗ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::zoned,
+    expected => '(⊙_◎)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::crazy,
+    expected => 'ミ●﹏☉ミ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::trolling,
+    expected => ' ༼∵༽ ༼⍨༽ ༼⍢༽ ༼⍤༽'
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::angry_troll,
+    expected => 'ヽ༼ ಠ益ಠ ༽ﾉ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::hugger,
+    expected => '(づ￣ ³￣)づ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::stranger_danger,
+    expected => '(づ｡◕‿‿◕｡)づ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::flip_friend,
+    expected => '(ノಠ ∩ಠ)ノ彡( \o°o)\ ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::cry,
+    expected => '｡ﾟ( ﾟஇ‸இﾟ)ﾟ｡',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::tgif,
+    expected => '“ヽ(´▽｀)ノ”',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::dancing,
+    expected => '┌(ㆆ㉨ㆆ)ʃ',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::sleepy,
+    expected => '눈_눈',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::shy,
+    expected => '(๑•́ ₃ •̀๑)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::fly_away,
+    expected => '⁽⁽ଘ( ˊᵕˋ )ଓ⁾⁾',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::careless,
+    expected => '◔_◔',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::love,
+    expected => '♥‿♥',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::touchy,
+    expected => 'ԅ(≖‿≖ԅ)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::robot,
+    expected => '{•̃_•̃}',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::seal,
+    expected => '(ᵔᴥᵔ)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::questionable,
+    expected => '(Ծ‸ Ծ)',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::winning,
+    expected => '(•̀ᴗ•́)و ̑̑',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::zombie,
+    expected => '[¬º-°]¬',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::pointing,
+    expected => '(☞ﾟヮﾟ)☞',
+);
+
+moon_test_one(
+    test => 'scalar',
+    meth => \&ACME::AsciiEmoji::chasing,
+    expected => "''⌐(ಠ۾ಠ)¬'''",
 );
 
 sunrise();
